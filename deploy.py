24e2c78b96928e7e2565c77094a26d9c17cf5c29
root <root@ip-172-31-11-120.eu-west-1.compute.internal>
2017-10-07 20:00:40 +0000

    Added functionality to run application container

--- deploy.py
+++ deploy.py
@@ -32,9 +32,15 @@ def build_application_container(client, tag='pizza-express'):
     response = [line for line in client.build(tag=tag, path='./')]
     return response
     
-def run_application_container(client, image='pizza-express:latest'):
+def run_application_container(client, redis_container_name, image='pizza-express:latest'):
+    config = client.create_host_config(port_bindings={'3000': '8081'}, links=[(redis_container_name, 'redis')])
+    container = client.create_container(image=image, host_config=config)
+    response = client.start(container=container.get('Id'))
+    return container
+
+def check_application_is_working():
     pass
-    
+
 def send_docker_image():
     pass
 
@@ -47,7 +53,7 @@ def main():
     redis_container_id, redis_container_name = run_redis_container(client)
 #   print redis_container_id, redis_container_name
     build_result = build_application_container(client)
-    
+    app_container_id = run_application_container(client, redis_container_name) 
 #   print build_result[-1].split('"')[-2].replace('\\n', '') 
             
 if __name__ == '__main__':
