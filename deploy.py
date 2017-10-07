ed1e2dd72a8489f705f35287ec5735572a8f5f7f
root <root@ip-172-31-11-120.eu-west-1.compute.internal>
2017-10-07 17:20:46 +0000

    Added functionality to build app container

--- deploy.py
+++ deploy.py
@@ -1,68 +1,54 @@
 #!/usr/bin/python
 
 import docker
-import commands
 
+def check_needed_images_exist(client, needed_imgs=['redis:latest', 'node:latest']):
+    docker_images = []
+    need_to_pull = []
+    for image in client.images():
+        docker_images.append(image["RepoTags"][0])
 
-def check_images(client, needed_imgs=['redis', 'node']):
-    downloaded_images = []
-    for image in client.images.list():
-        print image
-        downloaded_images.append(image.attrs["RepoTags"][0].split(":")[0])
+    for image in needed_imgs:        
+        if image not in docker_images:
+             need_to_pull.append(image)
 
-    for image in needed_imgs:
-        if image not in downloaded_images:
-            print "[!] Pulling {0} container".format(image)
-            client.images.pull('{0}:latest'.format(image))
+    return need_to_pull
+
+def pull_docker_images(client, need_to_pull):
+    for image in need_to_pull:
+        client.pull(image)
+
+def run_redis_container(client, image='redis:latest'):
+    config = client.create_host_config(port_bindings={'6379': '6379'})
+    container = client.create_container(image=image, host_config=config)
+    response = client.start(container=container.get('Id'))
+    for started_container in client.containers():
+        if started_container['Id'] == container['Id']:
+            return started_container['Id'], started_container['Names'][0].replace("/", "")
         else:
-            print "[+] Image {0} is present".format(image)
+            continue
 
-def check_redis_running(client, redis_container_name='redis'):
-    ran_containers = []
-    containers_list = client.containers.list()
-    for container in containers_list:
-        ran_containers.append(container.attrs['Name'].split('/')[1])
-    if redis_container_name in ran_containers:
-        return True
-    else:
-        return False
-
-def run_tests():
-    pass
-
-
-def run_redis_container():
-    output = commands.getstatusoutput('docker run -d --name redis -p 6379:6379 redis')
-    return output
-
-def run_container(**kwargs):
-    if not kwargs['image']:
-        print "[-] Print please provide container name"
-        exit(1)
+def build_application_container(client, tag='pizza-express'):
+    response = [line for line in client.build(tag=tag, path='./')]
+    return response
     
-    if kwargs['container_port'] and kwargs['host_port']:
-        
-    output = commands.getstatusoutput()
-
-def run_unittests():
-    output = commands.getstatusoutput('npm test')
-    return output
-
-def run_container_with_application():
-
+def run_application_container(client, image='pizza-express:latest'):
+    pass
+    
 def send_docker_image():
     pass
 
 def main():
-    client = docker.from_env(version='auto')
-    check_images(client)
-    redis_running = check_redis_running(client)
-    if not redis_running:
-        redis = run_redis_container()
-    unittests_passed = run_unittests()
-    if unittests_passed[0] == 0:
-            
-    
+    client = docker.Client(version='auto')
+    needed_images = check_needed_images_exist(client)
+    if needed_images:
+        pull_docker_images(client, needed_images)
 
+    redis_container_id, redis_container_name = run_redis_container(client)
+#   print redis_container_id, redis_container_name
+    build_result = build_application_container(client)
+    
+#   print build_result[-1].split('"')[-2].replace('\\n', '') 
+            
 if __name__ == '__main__':
     main()
